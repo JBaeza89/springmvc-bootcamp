aeeb03f22e1980e4411870b0b0f55d5c6f3cf73b
Jorge Baeza <jorgembaeza@gmail.com>
2020-05-17 14:43:22 +0200

    Rename and change extended interface

--- src/main/java/net/aspanc/bootcamp/springmvc/daos/IGameDao.java
+++
@@ -1,7 +0,0 @@
-package net.aspanc.bootcamp.springmvc.daos;
-
-import net.aspanc.bootcamp.springmvc.entities.Game;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IGameDao extends CrudRepository<Game, Long> {
-}
