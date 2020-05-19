bf60a6a39db1eb31821017d31eab3eca23936ef1
Jorge Baeza <jorgembaeza@gmail.com>
2020-05-19 19:53:00 +0200

    Added @NonNull config and improve game service test

--- src/test/java/net/aspanc/bootcamp/springmvc/services/DefaultGameServiceIntegrationTest.java
+++ src/test/java/net/aspanc/bootcamp/springmvc/services/DefaultGameServiceIntegrationTest.java
@@ -1,12 +1,13 @@
 package net.aspanc.bootcamp.springmvc.services;
 
-import net.aspanc.bootcamp.springmvc.daos.GameDao;
 import net.aspanc.bootcamp.springmvc.entities.Game;
 import org.junit.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -15,14 +16,20 @@ public class DefaultGameServiceIntegrationTest {
     @Autowired
     private GameService gameService;
 
-    private Game game1;
+    private final Game GAME1 = new Game("Fifa 20", "futbol", "f2020");
 
-    private Game game2;
+    private final Game GAME2 = new Game("NBA 2k20", "basket", "2k20");
+
 
     @Before
     public void setUp() {
-        game1 = gameService.save(new Game("Fifa 20", "futbol", "f2020"));
-        game2 = gameService.save(new Game("NBA 2k20", "basket", "2k20"));
+        if (GAME1.getId() == null && GAME2.getId() == null) {
+            GAME1.setId(gameService.save(GAME1).getId());
+            GAME2.setId(gameService.save(GAME2).getId());
+        } else {
+            gameService.save(GAME1);
+            gameService.save(GAME2);
+        }
     }
 
     @After
@@ -33,16 +40,15 @@ public class DefaultGameServiceIntegrationTest {
     @Test
     public void create() {
         final Game game = gameService.save(new Game("Witcher 3", "Epic fantasy", "GR77"));
-        Assert.assertTrue("Optional is Empty", gameService.findOne(game.getId()).isPresent());
-        Assert.assertEquals("The game list haven't include the saved game", 3, gameService.findAll().size());
+        Assert.assertTrue("Game are not in DataBase", gameService.existById(game.getId()));
     }
 
     @Test
     public void retrieveByID() {
-        gameService.findOne(game1.getId())
+        gameService.findOne(GAME1.getId())
                 .ifPresentOrElse(game -> {
-                    Assert.assertEquals("Title isn't equal", game1.getTitle(), game.getTitle());
-                    Assert.assertEquals("Description isn't equal", game1.getDescription(), game.getDescription());
+                    Assert.assertEquals("Title isn't equal", GAME1.getTitle(), game.getTitle());
+                    Assert.assertEquals("Description isn't equal", GAME1.getDescription(), game.getDescription());
                 }, () -> {
                     Assert.fail();
                 });
@@ -50,7 +56,9 @@ public class DefaultGameServiceIntegrationTest {
 
     @Test
     public void retrieveByTitle() {
-        Assert.assertEquals("Filter list size doesn't match",1, gameService.findByQuery("Fifa").size());
+        final String FILTER = "Fifa";
+        gameService.findByQuery(FILTER)
+                .forEach(game -> Assert.assertTrue("Title doesn't contains filter", game.getTitle().contains(FILTER)));
     }
 
     @Test
@@ -60,27 +68,20 @@ public class DefaultGameServiceIntegrationTest {
 
     @Test
     public void update() {
-        gameService.findOne(game2.getId())
-                .ifPresentOrElse(game -> {
-                    Assert.assertEquals("Changes are not in DataBase", "basket", game.getDescription());
-                }, () -> {
-                    Assert.fail();
-                });
-        game2.setDescription("Basket MVP");
-        gameService.save(game2);
-        gameService.findOne(game2.getId())
+        Assert.assertEquals("Description doesn't march", "basket", GAME2.getDescription());
+        GAME2.setDescription("Basket MVP");
+        gameService.save(GAME2);
+        gameService.findOne(GAME2.getId())
                 .ifPresentOrElse(game -> {
             Assert.assertEquals("Changes are not in DataBase", "Basket MVP", game.getDescription());
         }, () -> {
                     Assert.fail();
                 });
-        Assert.assertEquals(2, gameService.findAll().size());
     }
 
     @Test
     public void remove() {
-        Assert.assertTrue("Game with that Id not found", gameService.existById(game1.getId()));
-        gameService.remove(game1.getId());
-        Assert.assertFalse("Game with that ID still in DataBase", gameService.existById(game1.getId()));
+        gameService.remove(GAME1.getId());
+        Assert.assertFalse("Game with that ID still in DataBase", gameService.existById(GAME1.getId()));
     }
 }
