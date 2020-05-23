2a93da60e0c01880023b54d7ce2d037055934708
Jorge Baeza <jorgembaeza@gmail.com>
2020-05-23 06:45:32 +0200

    Added value and method to @RequestMapping

--- src/main/java/net/aspanc/bootcamp/springmvc/controller/GameController.java
+++ src/main/java/net/aspanc/bootcamp/springmvc/controller/GameController.java
@@ -2,13 +2,13 @@ package net.aspanc.bootcamp.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class GameController {
 
-    @RequestMapping
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String showIndex() {
-
         return "index";
     }
 }
