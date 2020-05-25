8849a2527dc9a8eea94669697044b07e4c5d4f9a
Jorge Baeza <jorgembaeza@gmail.com>
2020-05-25 12:28:30 +0200

    Listed stored games in index.jsp

--- src/main/java/net/aspanc/bootcamp/springmvc/controller/GameController.java
+++ src/main/java/net/aspanc/bootcamp/springmvc/controller/GameController.java
@@ -1,14 +1,23 @@
 package net.aspanc.bootcamp.springmvc.controller;
 
+import net.aspanc.bootcamp.springmvc.facade.GameFacade;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class GameController {
 
+    private GameFacade gameFacade;
+
+    public GameController(GameFacade gameFacade) {
+        this.gameFacade = gameFacade;
+    }
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String showIndex() {
+    public String showIndex(Model model) {
+        model.addAttribute("gameList", gameFacade.findAll());
         return "index";
     }
 }
