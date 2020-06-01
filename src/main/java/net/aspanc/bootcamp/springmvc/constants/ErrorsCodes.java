533ca9351586e49eebad9a2ae31222b9e7ba24cf
Jorge Baeza <jorgembaeza@gmail.com>
2020-06-01 18:38:57 +0200

    Reorganized validation build

--- src/main/java/net/aspanc/bootcamp/springmvc/constants/ErrorsCodes.java
+++
@@ -1,6 +0,0 @@
-package net.aspanc.bootcamp.springmvc.constants;
-
-public class ErrorsCodes {
-    public static final String EMPTY_GAME = "000";
-    public static final String EXIST_GAME = "001";
-}
