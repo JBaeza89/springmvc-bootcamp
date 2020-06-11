82bfa275b0cbf727ca505f16b565d9efd0f80991
Jorge Baeza <jorgembaeza@gmail.com>
2020-06-11 18:22:53 +0200

    fixed security code

--- src/main/java/net/aspanc/bootcamp/springmvc/configuration/AppWebSecurity.java
+++ src/main/java/net/aspanc/bootcamp/springmvc/configuration/AppWebSecurity.java
@@ -1,9 +1,6 @@
 package net.aspanc.bootcamp.springmvc.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,9 +12,8 @@ public class AppWebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/", "/js", "/css", "/game").permitAll()
-                .antMatchers("/game/new", "/game/delete/*", "/game/edit/*", "/admin/rest/*")
-                .hasAnyAuthority("REGISTERED")
-                .and().formLogin();
+            .antMatchers("/", "/js", "/css", "/game").permitAll()
+            .antMatchers("/game/new", "/game/delete/**", "/game/edit/**", "/admin/rest/**").hasAnyRole("REGISTERED")
+            .and().formLogin();
     }
 }
