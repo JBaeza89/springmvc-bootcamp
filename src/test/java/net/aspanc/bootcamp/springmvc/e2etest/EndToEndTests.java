0e1c548384bfc945318573231d831f1988612d67
Jorge Baeza <jorgembaeza@gmail.com>
2020-06-17 15:04:42 +0200

    fixed context error in tests

--- src/test/java/net/aspanc/bootcamp/springmvc/e2etest/EndToEndTests.java
+++ src/test/java/net/aspanc/bootcamp/springmvc/e2etest/EndToEndTests.java
@@ -3,11 +3,14 @@ package net.aspanc.bootcamp.springmvc.e2etest;
 import com.codeborne.selenide.Configuration;
 import net.aspanc.bootcamp.springmvc.SpringmvcApplication;
 import net.aspanc.bootcamp.springmvc.categories.End2EndTest;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static com.codeborne.selenide.Selenide.*;
@@ -15,13 +18,20 @@ import static com.codeborne.selenide.Selenide.*;
 
 @Category(End2EndTest.class)
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class EndToEndTests {
 
+    @LocalServerPort
+    private String serverPort;
 
     @BeforeClass
-    public static void setUp() {
+    public static void initSetUp() {
         Configuration.browser = "firefox";
+    }
+
+    @Before
+    public void setUp() {
+        Configuration.baseUrl= "http://localhost:" + serverPort;
     }
     @Test
     public void checkElementInBaseUrl() {
